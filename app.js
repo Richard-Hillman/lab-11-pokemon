bb36d85fbd32c32c6b0b528574c3f7ebcdf33247
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-10-12 21:48:39 -0700

    new

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // import functions and grab DOM elements
 import { pokemonData } from './data.js';
-import { getRandomPokemon } from './utils.js';
+import { refreshPoke } from './utils.js';
 
-const radioButtons = document.querySelectorAll('input');
-const images = document.querySelectorAll('label > img');
+
 const capture = document.querySelector('#capture-button');
-
+const capturedPokemon = document.querySelector('rando-pokemon');
 
 
 // initialize states
 const pokemon = pokemonData.slice();
+
 let capturedPoke = 0;
 let encounteredPoke = 0; 
 
@@ -18,6 +18,8 @@ let missedPokemonArray = [];
 
 // set event listeners to upddate state and DOM
 
+refreshPoke();
+
 
 capture.addEventListener('click', () => {
     capturedPoke++;
@@ -25,37 +27,7 @@ capture.addEventListener('click', () => {
     encounteredPoke++;
     encounteredPoke++;
     refreshPoke();
+    
 });
 
 
-function refreshPoke() {
-
-    let pokeOne = getRandomPokemon(pokemonData);
-    let pokeTwo = getRandomPokemon(pokemonData);
-    let pokeThree = getRandomPokemon(pokemonData);
-
-    while (pokeOne.id === pokeTwo.id) {
-        pokeOne = getRandomPokemon(pokemonData);
-    }
-
-    while (pokeTwo.id === pokeThree.id) {
-        pokeTwo = getRandomPokemon(pokemonData);
-    }
-
-    while (pokeThree.id === pokeOne.id) {
-        pokeThree = getRandomPokemon(pokemonData);
-    }
-
-    radioButtons[0].value = pokeOne.id;
-    images[0].src = pokeOne.url_image;
-
-
-    radioButtons[1].value = pokeTwo.id;
-    images[1].src = pokeTwo.url_image;
-
-
-    radioButtons[2].value = pokeThree.id;
-    images[2].src = pokeThree.url_image;
-}
-
-refreshPoke();
